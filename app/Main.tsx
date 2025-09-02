d8fc513c1a94f80a48b6dfc43e2f399c22bbab39
tinnitusi <tinnitusi@ndy.onl>
2025-09-02 10:09:34 +0200

    feat: Integrate core package for testing

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,6 +3,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { getCoreGreeting } from '@ndy-onl/nextjs-tina-core'
 
 const MAX_DISPLAY = 5
 
@@ -14,6 +15,7 @@ export default function Home({ posts }) {
           <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
             Latest
           </h1>
+          <p className="text-lg leading-7 text-green-500">{getCoreGreeting()}</p>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
